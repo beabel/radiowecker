f867c7d49f789fc6a54910b9cc3c0f7ce83d4c8f
kunigunde <selfcommerce@gmail.com>
2024-08-16 20:25:15 +0200

    Korrektur Bib einbindung
    externe mit <> einbinden

--- radiowecker/00_librarys.h
+++ radiowecker/00_librarys.h
@@ -3,8 +3,8 @@
 #include <ArduinoOTA.h>
 
 // TFT & Touchscreen Bibliotheken
-#include "Adafruit_GFX.h"         // Basisbibliothek für Grafikanzeige
-#include "Adafruit_ILI9341.h"     // Treiberbibliothek für das ILI9341 TFT-Display
+#include <Adafruit_GFX.h>         // Basisbibliothek für Grafikanzeige
+#include <Adafruit_ILI9341.h>     // Treiberbibliothek für das ILI9341 TFT-Display
 #include <XPT2046_Touchscreen.h>  // Treiberbibliothek für das XPT2046 Touchscreen
 #include <TouchEvent.h>           // Bibliothek zur Analyse von Touchscreen-Ereignissen
 
@@ -16,7 +16,7 @@
 
 // Bibliotheken für Audio-Funktionen
 // Enthält Klassen für die Audiowiedergabe und -verarbeitung
-#include "AudioFileSourceICYStream.h"  // Klasse für den Eingabestream von ICY (Internet Radio) Streams
-#include "AudioFileSourceBuffer.h"     // Klasse für das Puffern von Audio-Daten
-#include "AudioGeneratorMP3.h"         // MP3-Decoder Klasse
-#include "AudioOutputI2S.h"            // Klasse für die Audioausgabe über I2S (Inter-IC Sound) Schnittstelle
+#include <AudioFileSourceICYStream.h>  // Klasse für den Eingabestream von ICY (Internet Radio) Streams
+#include <AudioFileSourceBuffer.h>     // Klasse für das Puffern von Audio-Daten
+#include <AudioGeneratorMP3.h>         // MP3-Decoder Klasse
+#include <AudioOutputI2S.h>            // Klasse für die Audioausgabe über I2S (Inter-IC Sound) Schnittstelle
