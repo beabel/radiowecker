640ba1274e4a5a13f4c5b35828a6413c01e53d0e
kunigunde <github@ne-xt.de>
2024-08-30 04:57:04 +0200

    dev 306

--- radiowecker/radiowecker.ino
+++ radiowecker/radiowecker.ino
@@ -1,4 +1,4 @@
-#define RADIOVERSION "v3.0.5";
+#define RADIOVERSION "v3.0.6";
 #include "00_librarys.h"      //Lade alle benötigten Bibliotheken
 #include "00_pin_settings.h"  //Einstellungen der genutzten Pins
 #include "00_settings.h"      //einstellungen
