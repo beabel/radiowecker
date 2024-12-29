130916540e0164752d1ec7a86ded008807616619
kunigunde <admin@ne-xt.de>
2024-12-29 10:37:38 +0100

    Versionsnummer Update
    keine Änderung

--- radiowecker/radiowecker.ino
+++ radiowecker/radiowecker.ino
@@ -1,4 +1,4 @@
-#define RADIOVERSION "v4.0.0";
+#define RADIOVERSION "v4.0.1";
 #include "00_librarys.h"      //Lade alle benötigten Bibliotheken
 #include "00_pin_settings.h"  //Einstellungen der genutzten Pins
 #include "00_settings.h"      //einstellungen
