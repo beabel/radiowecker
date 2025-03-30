8f502ae87fd773994f388a6b5c89104a20390e94
kunigunde <github@ne-xt.de>
2025-03-30 10:13:42 +0200

    Update radiowecker.ino Version Nummer

--- radiowecker/radiowecker.ino
+++ radiowecker/radiowecker.ino
@@ -1,4 +1,4 @@
-#define RADIOVERSION "v4.0.2";
+#define RADIOVERSION "v4.0.3";
 #include "00_librarys.h"      //Lade alle benötigten Bibliotheken
 #include "00_pin_settings.h"  //Einstellungen der genutzten Pins
 #include "00_settings.h"      //einstellungen
